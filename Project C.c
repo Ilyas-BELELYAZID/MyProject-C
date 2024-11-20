1cda5f744f4c0e724754929e40f525000b7db2f5
Ilyas BEL EL YAZID <belelyazidilyas@gmail.com>
2024-11-21 00:19:03 +0100

    Modification and Add one function to the programme

---
+++ Project C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include <time.h>
+#include <windows.h>
+#include "FONCTION.H"
+
+void main()
+{
+
+    espace_etudiant();
+    system("cls");
+    espace_administration();
+    getch();
+}
